4d670ae95c6e6cfadd0c7155dfa30c5815108bb0
Rachel Trana <raxytrana@gmail.com>
2025-12-06 19:28:07 -0600

    AOCDAY05 - Part I

---
+++ src/main/kotlin/AOCDay05.kt
@@ -0,0 +1,37 @@
+import java.math.BigInteger
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+class AOCDay05(val input: List<String>) {
+
+    var ranges = mutableListOf<List<BigInteger>>()
+    val ingredients = mutableListOf<BigInteger>()
+
+    init {
+        val lines = input.map { it.trim() }.filter { it.isNotEmpty() }
+
+        for (line in lines) {
+            if ("-" in line) {
+                val (start, end) = line.split("-").map { it.toBigInteger() }
+                this.ranges += listOf(start, end)
+            } else {
+                this.ingredients += line.toBigInteger()
+            }
+        }
+        this.ranges = this.ranges.sortedBy { it.first() }.toMutableList()
+    }
+
+    fun countFreshIngredients(): Long {
+        var count = 0L
+        for (ingredient in this.ingredients) {
+            this.ranges.find { ingredient >= it.first() && ingredient <= it[1] }?.let { count++ }
+        }
+        return count
+    }
+}
+
+fun main() {
+    val fileInput = Path("src/main/resources/inputDay05a.txt").readLines()
+    val day05 = AOCDay05(fileInput)
+    println(day05.countFreshIngredients())
+}
